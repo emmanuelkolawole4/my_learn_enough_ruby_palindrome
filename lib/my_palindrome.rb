fe4ec2bd20f402262d8e8067329c17101ea1e7aa
Folahanmi Kolawole <emmanuelkolawole4@gmail.com>
2023-12-28 15:27:39 +0100

    Finish working and refactored palindrome method

--- lib/my_palindrome.rb
+++ lib/my_palindrome.rb
@@ -2,7 +2,21 @@
 
 require_relative "my_palindrome/version"
 
-module MyPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+# module MyPalindrome
+#   class Error < StandardError; end
+#   # Your code goes here...
+# end
+
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  private def processed_content
+    self.scan(/[a-z]/i).join.downcase
+  end
+
+  # Returns content for palindrome testing.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
 end
+  
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,6 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "my_palindrome"
-
 require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use!
