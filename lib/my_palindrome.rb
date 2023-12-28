9547e9c429870ba25fd8b7b8ded5a7945bea99e3
Folahanmi Kolawole <emmanuelkolawole4@gmail.com>
2023-12-28 15:37:30 +0100

    Finish working and refactored palindrome method

--- lib/my_palindrome.rb
+++ lib/my_palindrome.rb
@@ -9,14 +9,16 @@ require_relative "my_palindrome/version"
 
 class String
 
-  # Returns true for a palindrome, false otherwise.
-  private def processed_content
-    self.scan(/[a-z]/i).join.downcase
-  end
-
   # Returns content for palindrome testing.
   def palindrome?
     processed_content == processed_content.reverse
+  end
+
+  private
+
+  # Returns true for a palindrome, false otherwise.
+  def processed_content
+    scan(/[a-z]/i).join.downcase
   end
 end
   
